2a89d7d087ac2e3bf797f8dfc0c85d4ea7883565
Cheberi <meshackkemboi011@gmail.com>
2022-04-26 10:45:20 +0300

    added a user interface

--- passlock.py
+++ passlock.py
@@ -1,3 +1,10 @@
+import string
+import pyperclip
+import random
+
+
+
+
 class User:
     """
     Class that generates new instance of a user
@@ -63,5 +70,47 @@ class Credentials():
        """
        method to store new credentials in the credentials list
        """ 
+       Credentials.credentials_list.append(self)
 
-       
+    def delete_credentials(self):
+        """
+        method that deletes an account's credentials for a certain account
+        """
+        Credentials.credentials_list.remove(self)
+
+    @classmethod
+    def find_credential(cls, account):
+        """
+        Method that take in account name and return credentials that matches the account name.
+        """
+        for credential in cls.credentials_list:
+            if credential.account == account:
+                return credential
+    @classmethod
+    def copy_password(cls, account):
+        found_credentials = Credentials.find_credentials(account)
+        pyperclip.copy(found_credentials.password)
+
+    @classmethod
+    def if_credential_exists(cls, account):
+        """
+        Method that checks if a credential exist and returns true or false.
+        """
+        for credential in cls.credentials_list:
+            if credential.account == account:
+                return True
+        return False
+
+    @classmethod
+    def display_credentials(cls):
+        """
+        Method that returns all credentials in credential list
+        """
+        return cls.credentials_list
+
+    def genPassword(stringLength=8):
+         """
+         Generate a random password string of letters and digits and special characters
+         """
+         password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
+         return ''.join(random.choice(password) for i in range(stringLength))
